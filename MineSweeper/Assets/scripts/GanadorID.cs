9330e76b20fe685462058ee44b7912a6bb79e119
AlexandraAlbarracin <alexandra.albarracin@ucsp.edu.pe>
2018-05-13 19:18:59 -0500

    GanadorID/Boton Salir

---
+++ MineSweeper/Assets/scripts/GanadorID.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using AssemblyCSharp;
+
+public class GanadorID : MonoBehaviour
+{
+    public Text m_MyText;
+    
+    void Start()
+    {
+        //Text sets your text to say this message
+        m_MyText.text = "Ganador: " + AssemblyCSharp.PlayerInfo.Instance.player_winner;
+    }
+
+    void Update()
+    {
+    }
+}
