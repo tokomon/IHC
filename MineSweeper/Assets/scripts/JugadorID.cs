ce5251e08160ea0c0828bb1cacf30ab573ce73b4
AlexandraAlbarracin <alexandra.albarracin@ucsp.edu.pe>
2018-06-19 01:10:10 -0500

    zoom/reconexion

---
+++ MineSweeper/Assets/scripts/JugadorID.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using AssemblyCSharp;
+
+public class JugadorID : MonoBehaviour {
+
+    public Text m_MyText;
+    // Use this for initialization
+    void Start () {
+        m_MyText.text = "Jugador: " + AssemblyCSharp.PlayerInfo.Instance.player_id  ;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ MineSweeper/Assets/scripts/Reconexion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Reconexion : MonoBehaviour
+{
+
+    public void cambiarescena(string nombre)
+    {
+        InputField IPField = (InputField)GameObject.Find("IP").GetComponent<InputField>();
+        InputField PuertoField = (InputField)GameObject.Find("Puerto").GetComponent<InputField>();
+        InputField IDField = (InputField)GameObject.Find("ID").GetComponent<InputField>();
+
+        int puerto;
+        Int32.TryParse(PuertoField.text, out puerto);
+        int id;
+        Int32.TryParse(IDField.text, out id);
+        /*
+                bool data; //= AssemblyCSharp.PlayerInfo.Instance.startConnection(IPField.text, puerto, opcion);
+
+                if (data)
+                {
+                    SceneManager.LoadScene(1);
+                }
+                else
+                    Debug.Log("Imposible reconectar al servidor, pruebe otra vez PE");*/
+    }
+}
