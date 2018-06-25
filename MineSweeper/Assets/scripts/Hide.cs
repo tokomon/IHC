b05bbe76ffc1ff6f36528ec856c6f1af44e2463e
Angie <ang.18.vc@gmail.com>
2018-06-25 18:04:23 -0500

    Fix

--- MineSweeper/Assets/scripts/Hide.cs
+++ MineSweeper/Assets/scripts/Hide.cs
@@ -5,20 +5,7 @@ using UnityEngine;
 public class Hide : MonoBehaviour {
 
     void OnMouseDown()
-    {
-<<<<<<< HEAD
-        
-        int pos_x;
-        int pos_y;
-        System.Int32.TryParse(gameObject.name[0].ToString(), out pos_x);
-        System.Int32.TryParse(gameObject.name[2].ToString(), out pos_y);
-        Debug.Log(gameObject.name.ToString());
-        Debug.LogFormat("X: {0}", pos_x);
-        Debug.LogFormat("Y: {0}", pos_y);
-        AssemblyCSharp.PlayerInfo.Instance.sendFreeSpace(pos_x,pos_y);
-        
-=======
-
+    { 
         int Pos_x = AssemblyCSharp.PlayerInfo.Instance.pos_x;
         int Pos_y = AssemblyCSharp.PlayerInfo.Instance.pos_y;
         System.Int32.TryParse(gameObject.name[0].ToString(), out Pos_x);
@@ -27,14 +14,11 @@ public class Hide : MonoBehaviour {
         Debug.LogFormat("X: {0}", Pos_x);
         Debug.LogFormat("Y: {0}", Pos_y);
         AssemblyCSharp.PlayerInfo.Instance.sendFreeSpace(Pos_x,Pos_y);
-        /*
->>>>>>> 8f3704a0d0cf71366d234b02dd22b12fc6589b01
+
         if (gameObject.name[gameObject.name.Length-1] != '1'){
             Destroy(gameObject);
                     SoundManagerScript.PlaySound("cube");
 
-           // this.GetComponent<Renderer>().material = tablero.matPiso;
-            //  gameObject.GetComponent<Renderer>().enabled = false;
         }
         else
         {     SoundManagerScript.PlaySound("error");
